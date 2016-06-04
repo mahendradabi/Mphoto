cdfa2e6092d34025574d71ab4a67834438b3c8c9
MAHI <MAHI>
2016-06-04 07:01:45 -0700

    Initial commit

---
+++ app/src/main/java/mphoto/mahi/mphoto/MainActivity.java
@@ -0,0 +1,38 @@
+package mphoto.mahi.mphoto;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+ActionBar ab;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (Toolbar)findViewById(R.id.appbar);
+        setSupportActionBar(myToolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+
+
+
+        return super.onOptionsItemSelected(item);
+    }
+}
